b6542ff38cdce729fcc6597d71c2df8077e7a235
AppaDev <appa_tv@outlook.com>
2022-09-19 09:06:33 +0200

    InteractionSystem
    now using Raycast instead of Collider

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,12 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameData : MonoBehaviour
 {
+    [Header("wallet")]
     [SerializeField] private float cash;
     [SerializeField] private float moneyOnCard;
+    // ReSharper disable once InconsistentNaming
+    private float price;
     
     public float Cash { get; set; }
     public float MoneyOnCard { get; set; }
+    public float Price { get; set; }
 }
