d6e915b65d70bd437ce4c1f47d7ea5d44516e1ed
AppaDev <appa_tv@outlook.com>
2022-09-19 14:46:44 +0200

    Fixing Everything

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -8,10 +8,16 @@ public class GameData : MonoBehaviour
     [Header("wallet")]
     [SerializeField] private float cash;
     [SerializeField] private float moneyOnCard;
-    // ReSharper disable once InconsistentNaming
+    // ReSharper disable InconsistentNaming
     private float price;
-    
+    /* UI-Charger */
+    private float chargeAmount;
+    private float newBalance;
+
     public float Cash { get; set; }
     public float MoneyOnCard { get; set; }
     public float Price { get; set; }
+ 
+    public float ChargeAmount { get; set; }
+    public float NewBalance { get; set; }
 }
